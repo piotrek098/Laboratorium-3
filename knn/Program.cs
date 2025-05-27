5f45d11c66d7ffc421718d0db04e61fb81f6f54e
piotrek098 <cichockip3@gmail.com>
2025-05-27 21:40:06 +0200

    Cały Knn

--- knn/Program.cs
+++ knn/Program.cs
@@ -6,30 +6,10 @@ using System.IO;
 
 class Program
 {
-    static Random random = new Random();
     public static void Main(string[] args)
     {
-        double[]A=new double[10];
-        double[]B=new double[10];
-        for(int i = 0; i < 10; i++)
-        {
-            A[i]=random.NextDouble();
-        }
-        for (int i = 0; i < 10; i++)
-        {
-            B[i] = random.NextDouble();
-        }
-
-        Metryka m = Euklidesowa;
-        double wynik=m(A,B);
-        Console.WriteLine(wynik);
-
-
-        Metryka n = Manhatan;
-        double wynik1 = n(A, B);
-        Console.WriteLine(wynik1);
-
-        string plik = @"C:\Users\Lenovo\Desktop\WS\zadanie 2\knn\dane.txt"; 
+        
+        string plik = @"C:\Users\Lenovo\Desktop\WS\zadanie 2\knn\dane.txt";
         var dane = new List<double[]>();
 
         foreach (var line in File.ReadLines(plik))
@@ -44,31 +24,36 @@ class Program
 
             dane.Add(row);
         }
-       
+
 
         Console.WriteLine("Pierwszy wiersz:");
         Console.WriteLine(string.Join(", ", dane[0]));
 
         Console.WriteLine($"\nWczytano {dane.Count} wierszy.");
 
-       
+
 
         List<double[]> probki = new List<double[]>();
 
-        foreach(var wiersz in dane)
+        foreach (var wiersz in dane)
         {
             probki.Add(new double[] { wiersz[0], wiersz[1], wiersz[2], wiersz[3] });
         }
-        for (int i = 0; i < probki.Count; i++)
-        {
-            Console.WriteLine($"Numer próbki {i+1}:");
-            for (int j = 0; j < probki[i].Length; j++)
-            {
-                Console.Write($"{probki[i][j]} ");
-            }
-            Console.WriteLine();
-        }
+
         
+        List<double[]> ostatniaKolumna = new List<double[]>();
+        foreach(var wiersz in dane)
+        {
+            ostatniaKolumna.Add(new double[] { wiersz[4] });
+        }
+        double[]nowy= new double[] { 1.2, 1.5, 5, 2.3 };
+        int k = 3;
+        Metryka m = Euklidesowa;
+        var wynik = Indeksynajmniejszych(probki, nowy, m, k);
+
+        double wynikKlasyfikacji = Klasyfikuj(ostatniaKolumna, wynik);
+        Console.WriteLine($"Przewidziana klasa: {wynikKlasyfikacji}");
+
     }
 
 
@@ -114,5 +99,68 @@ class Program
     }
 
     delegate double Metryka(double[] A, double[] B);
+
+
+    static int[] Indeksynajmniejszych(List<double[]> dane, double[] wektorTestowy, Metryka metryka,int ile)
+    {
+        double[] odleglosci = new double[dane.Count];
+
+        int[] indeksy = new int[dane.Count];
+
+
+        for (int i = 0; i < dane.Count; i++)
+        {
+            odleglosci[i] = metryka(dane[i], wektorTestowy);
+            indeksy[i] = i;
+        }
+        Array.Sort(odleglosci, indeksy);
+
+        for (int i = 0; i < ile; i++)
+        {
+            Console.WriteLine($"Indeks: {indeksy[i]}, Odległość: {odleglosci[i]}");
+        }
+        int[] najblizszeIndeksy = new int[ile];
+        for (int i = 0; i < ile; i++)
+        {
+            najblizszeIndeksy[i] = indeksy[i];
+        }
+        return najblizszeIndeksy;
+    }
+    static double Klasyfikuj(List<double[]> klasy, int[] indeksyNajblizszych)
+    {
+        double[] klasyNajblizszych = new double[indeksyNajblizszych.Length];
+
+        for (int i = 0; i < indeksyNajblizszych.Length; i++)
+        {
+            int indeks = indeksyNajblizszych[i];
+            klasyNajblizszych[i] = klasy[indeks][0]; 
+        }
+
+        double najczestsza = klasyNajblizszych[0];
+        int maxLicznik = 1;
+
+        for (int i = 0; i < klasyNajblizszych.Length; i++)
+        {
+            double aktualna = klasyNajblizszych[i];
+            int licznik = 0;
+
+            for (int j = 0; j < klasyNajblizszych.Length; j++)
+            {
+                if (klasyNajblizszych[j] == aktualna)
+                {
+                    licznik++;
+                }
+            }
+
+            if (licznik > maxLicznik)
+            {
+                maxLicznik = licznik;
+                najczestsza = aktualna;
+            }
+        }
+
+        return najczestsza;
+    }
+
 
 }
